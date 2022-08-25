2a7f54426432ac3aae7717f94f250669a4941821
Matías <matiasagbenitez@gmail.com>
2022-08-24 22:40:12 -0300

    Test cases added & Hashid package installed

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -17,4 +17,11 @@ class LinkFactory extends Factory
             'slug' => Str::random(6)
         ];
     }
+
+    public function ofUser(User $user)
+    {
+        return $this->state([
+            'user_id' => $user->id
+        ]);
+    }
 }
